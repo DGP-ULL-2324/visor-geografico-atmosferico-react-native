23e52a45cc4132d849a302d7f4f69c7d6ec0d457
MREugeneJ7 <32437774+MREugeneJ7@users.noreply.github.com>
2023-12-11 19:20:24 +0000

    DGP2324-85 Refactor into functions

--- visor-geografico-atmosferico/external-services/weather-api.mjs
+++ visor-geografico-atmosferico/external-services/weather-api.mjs
@@ -1,25 +1,60 @@
 import settings from '../config/external-api.json' assert { type: "json" };
 
-let stations = []
-
-stations = await fetch(settings.baseUrl + settings.stationsEndpoint)
-    .then(response => response.json())
-    .then(json => {
-        return json.stations.map(fullObject => {
-            return {
-                "id": fullObject.id_weatherstation,
-                "municipio": fullObject.municipality_name,
-                "localidad": fullObject.locality,
-                "latitud": fullObject.latitude,
-                "longitud": fullObject.longitude,
-                "altitud": fullObject.altitude,
-                "sensors": []
-            }
+async function loadSensorlessStations() {
+    return fetch(settings.baseUrl + settings.stationsEndpoint)
+        .then(response => response.json())
+        .then(json => {
+            return json.stations.map(fullObject => {
+                return {
+                    "id": fullObject.id_weatherstation,
+                    "municipio": fullObject.municipality_name,
+                    "localidad": fullObject.locality,
+                    "latitud": fullObject.latitude,
+                    "longitud": fullObject.longitude,
+                    "altitud": fullObject.altitude,
+                    "sensors": []
+                }
+            })
         })
-    })
+}
 
-for (let station of stations) {
-    station.sensors = await fetch(settings.baseUrl + settings.sensorsEndpoint.replace("{idWeatherstation}", station.id))
+async function addSensorsAndMeassuresToStations(stations) {
+    for (let station of stations) {
+        await getStationSensors(station)
+            .then(async sS => {
+                station.sensors = sS
+                getStationSensorReadings(station)
+                    .then(sR => addReadingsToSensors(station, sR))
+            })
+    };
+    return stations
+}
+
+async function addReadingsToSensors(station, readings) {
+    station.sensors.forEach(element => {
+        let medida = readings
+            .filter(sR => sR.id_weatherstationsensor == element.id)
+            .map(sR => sR.dates)
+            .flat(Infinity)
+            .filter(sR => sR.is_validated)
+            .pop()
+        if (medida) {
+            element.medida = medida.values[0]
+        }
+    });
+}
+
+async function getStationSensorReadings(station) {
+    let year = new Date().getFullYear() + ""
+    let month = (new Date().getMonth() + 1) + ""
+
+    return fetch(settings.baseUrl + settings.readingsEnpoint.replace("{idWeatherstation}", station.id).replace("{year}", year).replace("{month}", month))
+        .then(response => response.json())
+        .then(json => json.summarized ? json.summarized.sensors : [])
+}
+
+async function getStationSensors(station) {
+    return fetch(settings.baseUrl + settings.sensorsEndpoint.replace("{idWeatherstation}", station.id))
         .then(response => response.json())
         .then(json => {
             return json.stations[0].sensors.map(sensor => {
@@ -31,28 +66,12 @@ for (let station of stations) {
                 }
             })
         })
+}
 
-    let year = new Date().getFullYear() + ""
-    let month = (new Date().getMonth() + 1) + ""
-    
-    let sensorsReadings = await fetch(settings.baseUrl + settings.readingsEnpoint.replace("{idWeatherstation}", station.id).replace("{year}", year).replace("{month}", month))
-        .then(response => response.json())
-        .then(json => json.summarized ? json.summarized.sensors : [])
+async function fullLoad() {
+    loadSensorlessStations()
+        .then(st => addSensorsAndMeassuresToStations(st))
+        .then(s => console.log(s[0].sensors[0]))
+}
 
-    station.sensors.forEach(element => {
-        let medida = sensorsReadings
-            .filter( sR => sR.id_weatherstationsensor == element.id)
-            .map(sR => sR.dates)
-            .flat(Infinity)
-            .filter(sR => sR.is_validated)
-            .pop()
-        if(medida){
-            element.medida = medida.values[0]
-        }
-    });
-};
-
-console.log(stations[0])
-console.log(stations[0].sensors[0])
-console.log(stations[0].sensors[0].medida)
-
+fullLoad()
