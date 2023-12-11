25865d79c9a5fa601876183cc639857149741fcd
MREugeneJ7 <32437774+MREugeneJ7@users.noreply.github.com>
2023-12-11 18:18:39 +0000

    DGP2324-85 - Obtenidos los ultimos datos de la api

--- visor-geografico-atmosferico/external-services/weather-api.mjs
+++ visor-geografico-atmosferico/external-services/weather-api.mjs
@@ -1,5 +1,58 @@
-const settings = require('../config/external-api.json')
+import settings from '../config/external-api.json' assert { type: "json" };
 
-console.log(settings)
+let stations = []
 
-fetch(settings.baseUrl + settings.stationsEndpoint).then(e => console.log(e))
+stations = await fetch(settings.baseUrl + settings.stationsEndpoint)
+    .then(response => response.json())
+    .then(json => {
+        return json.stations.map(fullObject => {
+            return {
+                "id": fullObject.id_weatherstation,
+                "municipio": fullObject.municipality_name,
+                "localidad": fullObject.locality,
+                "latitud": fullObject.latitude,
+                "longitud": fullObject.longitude,
+                "altitud": fullObject.altitude,
+                "sensors": []
+            }
+        })
+    })
+
+for (let station of stations) {
+    station.sensors = await fetch(settings.baseUrl + settings.sensorsEndpoint.replace("{idWeatherstation}", station.id))
+        .then(response => response.json())
+        .then(json => {
+            return json.stations[0].sensors.map(sensor => {
+                return {
+                    "id": sensor.id_weatherstationsensor,
+                    "nombre": sensor.sensor_name,
+                    "unidadMedida": sensor.unit,
+                    "medida": undefined
+                }
+            })
+        })
+
+    let year = new Date().getFullYear() + ""
+    let month = (new Date().getMonth() + 1) + ""
+    
+    let sensorsReadings = await fetch(settings.baseUrl + settings.readingsEnpoint.replace("{idWeatherstation}", station.id).replace("{year}", year).replace("{month}", month))
+        .then(response => response.json())
+        .then(json => json.summarized ? json.summarized.sensors : [])
+
+    station.sensors.forEach(element => {
+        let medida = sensorsReadings
+            .filter( sR => sR.id_weatherstationsensor == element.id)
+            .map(sR => sR.dates)
+            .flat(Infinity)
+            .filter(sR => sR.is_validated)
+            .pop()
+        if(medida){
+            element.medida = medida.values[0]
+        }
+    });
+};
+
+console.log(stations[0])
+console.log(stations[0].sensors[0])
+console.log(stations[0].sensors[0].medida)
+
